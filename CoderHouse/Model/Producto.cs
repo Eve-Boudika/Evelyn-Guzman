f1c01f5a314f5c86392375e5db35adc3d04c83a0
Evelyn <evelabohemia@gmail.com>
2022-07-14 21:10:55 -0300

    cambio nombre de variable

--- CoderHouse/Model/Producto.cs
+++ CoderHouse/Model/Producto.cs
@@ -12,7 +12,7 @@ namespace Model
         public string Descripcion { get; set; }
         public double Costo { get; set; }
         public double PrecioVenta { get; set; }
-        public int stock { get; set; }
+        public int Stock { get; set; }
         public int IdUsuario { get; set; }
 
 
